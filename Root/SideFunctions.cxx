176f4e928c61581c58448f00352ef69c957a088e
Christophe Goudet <goudet@lal.in2p3.fr>
2016-06-08 11:16:19 +0200

    first draft of new bootstrap

--- Root/SideFunctions.cxx
+++ Root/SideFunctions.cxx
@@ -15,6 +15,7 @@
 #include "TObject.h"
 #include <set>
 #include "TArrayD.h"
+#include "PlotFunctions/MapBranches.h"
 
 #define DEBUG 1
 using std::unique_ptr;
@@ -194,67 +195,38 @@ TTree* Bootstrap( vector< TTree* > inTrees, unsigned int nEvents, unsigned long 
   unsigned int totEntry = 0;
   for ( unsigned int iTree = 0; iTree < inTrees.size(); iTree++ ) totEntry += inTrees[iTree]->GetEntries();
   if ( !nEvents ) nEvents = totEntry;
+
   cout << "nEvents : " << nEvents << endl;
 
-  TClass *expectedClass;
-  EDataType expectedType;
+  vector<unsigned int > totEntriesIndex, selectedEventsIndex;
+  for ( unsigned int i =0; i<totEntriesIndex.size(); i++ ) totEntriesIndex.push_back( i );
 
-  do {  
-    for ( unsigned int iTree = 0; iTree < inTrees.size(); iTree++ ) {
-      cout << "iTree : " << iTree << endl;
-      TObjArray *branches = inTrees[iTree]->GetListOfBranches();
-
-      vector< double > varDouble( branches->GetEntries(), 0 );
-      vector< long long int > varLongLong( branches->GetEntries(), 0 );
-
-      for ( unsigned int iBranch = 0; iBranch < (unsigned int) branches->GetEntries(); iBranch++ ) {
-
-	( (TBranch*) (*branches)[iBranch])->GetExpectedType( expectedClass, expectedType );
-
-	if ( !expectedClass ) {
-	  switch ( expectedType ) { //documentation at https://root.cern.ch/doc/master/TDataType_8h.html#add4d321bb9cc51030786d53d76b8b0bd
-	  case 8 : {//double
-	    inTrees[iTree]->SetBranchAddress( (*branches)[iBranch]->GetName(), &varDouble[iBranch] );
-	    if ( !iTree ) outTree->Branch( (*branches)[iBranch]->GetName(), &varDouble[iBranch] );
-	    break;}
-	  case 16 :
-	    inTrees[iTree]->SetBranchAddress( (*branches)[iBranch]->GetName(), &varLongLong[iBranch] );
-	    if ( !iTree ) outTree->Branch( (*branches)[iBranch]->GetName(), &varLongLong[iBranch] );
-	    break;
-	  default :
-	    cout << "bootstrap not planned for type : " << expectedType << endl;
-	    cout << "branchName : " << (*branches)[iBranch]->GetName() << endl;
-	    exit(0);
-	  }//end switch
-	}//end if expectedClass
-	else {
-	  cout << "bootstrap not planned for handmade classes" << endl;
-	  exit(0);
-	}
-	//	if ( expectedClass )  delete expectedClass; expectedClass=0;
-      }//end iBranch
-
-      unsigned int nEntries = inTrees[iTree]->GetEntries();
-      for ( unsigned int iEvent = 0; iEvent < nEntries; iEvent++ ) {
-	if ( outTree->GetEntries() >= nEvents ) {
-	  cout << "nEvents : " << nEvents << endl;
-	  cout << "iEvent : " << iEvent << endl;
-	  break;
-	}
-	inTrees[iTree]->GetEntry( iEvent );
-
-	unsigned int nUse = rand.Poisson( 1.0*nEvents/totEntry );
-	//  cout << "nUse : " << nUse << endl;
-	for ( unsigned int iUse = 0; iUse < nUse; iUse++ ) outTree->Fill();
-
-      }//end for iEvent
-
-    }//end iTree
+  for ( unsigned int iEvent=0; iEvent<nEvents; iEvent++ ) {
+    unsigned int xEntry = floor( rand.Uniform( totEntriesIndex.size() ) );
+    selectedEventsIndex.push_back( totEntriesIndex[xEntry] );
+    totEntriesIndex[xEntry] = totEntriesIndex.back();
+    totEntriesIndex.pop_back();
   }
-  while ( outTree->GetEntries() < nEvents );
+  sort( selectedEventsIndex.begin(), selectedEventsIndex.end() );
+  reverse( selectedEventsIndex.begin(), selectedEventsIndex.end() );
 
-  //  outTree->Print();
-  cout << "entries : " << outTree->GetEntries() << endl;
+  MapBranches mapB;
+  unsigned int counter=0;
+  for ( auto vTree : inTrees ) {
+    mapB.LinkTreeBranches( vTree, outTree );
+    
+    unsigned int nEntries = vTree->GetEntries();
+    for ( unsigned int iEvent = 0; iEvent < nEntries; iEvent++ ) {
+
+      while ( selectedEventsIndex.back() < counter ) selectedEventsIndex.pop_back();
+      while ( selectedEventsIndex.back() == counter ) {
+	outTree->Fill();
+	selectedEventsIndex.pop_back();
+      }
+
+      counter ++;
+    }
+  }
 
   return outTree;
 

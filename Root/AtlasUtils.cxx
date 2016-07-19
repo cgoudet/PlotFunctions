0c83ddddad809244dfd7f67b84702bff9c0f7b02
Christophe Goudet <goudet@lal.in2p3.fr>
2016-07-19 13:13:55 +0200

    Draf 2 Plot pub note

--- Root/AtlasUtils.cxx
+++ Root/AtlasUtils.cxx
@@ -12,9 +12,10 @@
 using std::cout;
 using std::endl;
 
-void ATLAS_LABEL(Double_t x,Double_t y,Color_t color) 
+void ATLAS_LABEL(Double_t x,Double_t y,Color_t color ) 
 {
-  TLatex l; //l.SetTextAlign(12); l.SetTextSize(tsize); 
+  TLatex l; //l.SetTextAlign(12); 
+  //l.SetTextSize(tsize); 
   l.SetNDC();
   l.SetTextFont(72);
   l.SetTextColor(color);

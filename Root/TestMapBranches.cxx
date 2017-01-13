4de7fb4623c38cb01af5f402eea4dcb1013aaf42
cgoudet <goudetchristophe@gmail.com>
2017-01-13 11:33:05 +0100

    Start TetingLinkTreeBranches

--- Root/TestMapBranches.cxx
+++ Root/TestMapBranches.cxx
@@ -48,7 +48,31 @@ BOOST_AUTO_TEST_CASE( SetValTest) {
   BOOST_CHECK_EQUAL( mapBr.GetFloat("float"), valF );
   BOOST_CHECK_EQUAL( mapBr.GetDouble("double"), valD );
   BOOST_CHECK_EQUAL( mapBr.GetDouble("float"), static_cast<double>(valF) );
-  
+
+  list<string> keys;
+  list<string> testKeys { "int", "float", "double", "string" };
+  mapBr.GetKeys( keys );
+  BOOST_CHECK( keys==testKeys );
+}
+
+//=======================================
+BOOST_AUTO_TEST_CASE( LinkTreeBranchesTest ) {
+  MapBranches mapBr;
+
+  TTree *tree1=0, *tree2=0;
+  BOOST_CHECK_THROW( mapBr.LinkTreeBranches(tree1, tree2 ), invalid_argument );
+
+  double val;
+  tree1= new TTree( "tree1", "tree1" );
+  tree1->Branch( "branch11", &val );
+  tree1->Branch( "branch12", &val );
+
+  tree2= new TTree( "tree2", "tree2" );
+  tree2->Branch( "branch21", &val );
+  tree2->Branch( "branch22", &val );
+
+  delete tree1;
+  delete tree2;
 }
 
 BOOST_AUTO_TEST_SUITE_END()

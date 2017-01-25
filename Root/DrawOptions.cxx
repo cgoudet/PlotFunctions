c0227c5dd7b7512fdb6362cca495786dc0c9cc74
cgoudet <goudetchristophe@gmail.com>
2017-01-25 17:33:11 +0100

    --ammend

--- Root/DrawOptions.cxx
+++ Root/DrawOptions.cxx
@@ -441,11 +441,9 @@ void ChrisLib::DrawOptions::Draw( vector< TObject* > &inHist ) {
 
   // =========== PRINT LEGENDS AND LATEX
   double lineVal = GetLine();
-  cout << "lineVal : " << lineVal << endl;
   if( lineVal != -99
       && ( lineVal > rangeUserY.front() && lineVal < rangeUserY.back() )
       ) {
-    cout << "drawLine" << endl;
     double rangeMin = rangeUserX.size()== 2 ? rangeUserX[0] : minX;
     double rangeMax = rangeUserX.size()== 2 ? rangeUserX[1] : maxX;
     line.DrawLine( rangeMin , lineVal, rangeMax, lineVal);

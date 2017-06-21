d9018f9e0ea4566fd4673cadd835f777ab1c5f07
cgoudet <goudetchristophe@gmail.com>
2017-06-21 14:42:26 +0200

    Set error to 0 for inputType2

--- Root/PlotFunctions.cxx
+++ Root/PlotFunctions.cxx
@@ -314,7 +314,7 @@ void ChrisLib::TestInputs( const InputCompare &inputCompare ) {
   if ( outMode==OutMode::profile ) { errors.set(0); errors.set(2); errors.set(3);}
   if ( outMode==OutMode::graphErrors ) { errors.set(2); errors.set(3); }
   if ( outMode!=OutMode::none ) errors.set(4);
-  if ( outMode == OutMode::histMultipleBranch ) { errors.set(4);}
+  if ( outMode == OutMode::histMultiBranch ) { errors.set(4);}
   if ( doLabels && IsTH1( outMode ) ) errors.set(0,0);
 
   if ( errors.test(0) && xBinning.empty() && ( varMin.empty() || varMax.empty()) ) throw invalid_argument( "TestInput : No information for binning provided" );
@@ -640,4 +640,5 @@ void ChrisLib::FillFunctionHisto( TH1* filledHist, const unsigned int bin, const
   else throw runtime_error( "ChrisLib::FillFunctionHisto : code (" + std::to_string(code) + ") does not correspond to any possibility.");
 
   filledHist->SetBinContent(bin, oldValue);
+  filledHist->SetBinError(bin, 0);
 }
